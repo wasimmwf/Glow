68595bda403eb4fe1fcf63b96354d941562ec025
makram@pmintl.net <Md.Wasim.Akram@contracted.pmi.com>
2024-09-17 17:32:31 +0530

    Home Screen Loaded and Dropdown added

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-//import HomeScreen from '../glow/src/pages/HomeScreen/HomeScreen';
+import HomeScreen from '../glow/src/pages/HomeScreen/HomeScreen';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer initialRouteName="Glow" >
       <Stack.Navigator>
-        <Stack.Screen name="Glow" component={HomeScreen} />
+        <Stack.Screen name="Glow" component={HomeScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,31 +1,15 @@
 // Import libraries to create a component
 import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  ImageBackground,
-  TouchableHighlight,
-  PermissionsAndroid,
-  Switch,
-  Platform,
-  NativeModules
-} from 'react-native';
+import {StyleSheet,Text,View,ImageBackground,TouchableHighlight,NativeModules} from 'react-native';
 
-const { PermissionCheck } = NativeModules;
 const { PermissionFile } = NativeModules;
 
 // Themes and Styles
-import { Images, Colors, Fonts } from '../../themes';
+import { Colors, Fonts } from '../../themes';
 import PackageJson from '../../../package.json';
 import * as RNFS from "react-native-fs";
-import { Dropdown } from "react-native-material-dropdown";
-//const GlowFolder = RNFS.ExternalStorageDirectoryPath + "/GLOW/";
-// const GlowFolder = "/storage/emulated/0/Android/media/{$packagename}" + "/GLOW/";
+import Dropdown from 'react-native-select-dropdown';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const GlowFolder = "/storage/emulated/0/Android/media/GLOW/";
 
 // Create a component
@@ -42,85 +26,6 @@ class HomeScreen extends Component {
   }
 
   componentDidMount = async () => {
-
-    //   PermissionsAndroid.request(
-    //     PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-    // ).then(granted => {
-    //    //console.log("granted ->>> ",granted)
-    //     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-    //       //console.log("Glow Folder ->> ",GlowFolder)
-    //       RNFS.exists(GlowFolder).then((folderExist) => {
-    //         if (folderExist) {
-    //           //console.log('folder exist ->> ', folderExist);
-    //         } else {
-    //           RNFS.mkdir(GlowFolder)
-    //             .then((isDirectoryCreated) => {
-    //               //console.log('DirectoryCreated');
-    //             })
-    //             .catch((err) => {
-    //               console.log(err.message);
-    //             });
-    //         }
-    //       });
-    //     } else {
-    //         alert("No permission granted. Please allow storage permission.");
-    //     }
-    // }).catch(e => console.warn(e));
-
-    // };
-    // PermissionCheck.getAvailablePermissions().then(dictPermissionList => {
-    //   var arrPermission = dictPermissionList["PermissionList"];
-    //   console.log('Wasim ->>> ',arrPermission);
-    //   //console.log("Glow Folder ->> ",GlowFolder)
-    //   RNFS.exists(GlowFolder).then((folderExist) => {
-    //     if (folderExist) {
-    //       //console.log('folder exist ->> ', folderExist);
-    //     } else {
-    //       RNFS.mkdir(GlowFolder)
-    //         .then((isDirectoryCreated) => {
-    //           //console.log('DirectoryCreated');
-    //         })
-    //         .catch((err) => {
-    //           console.log(err.message);
-    //         });
-    //     }
-    //   });
-    // }).catch(e => console.warn(e));
-
-    // var ext = RNFS.ExternalStorageDirectoryPath
-    // var tem = RNFS.TemporaryDirectoryPath
-    // console.log('Wasim Path ->>>>>', ext,tem)
-
-    // try {
-    //   const granted = await PermissionsAndroid.requestMultiple([
-    //     PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-    //     PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-    //   ]);
-    // } catch (err) {
-    //   console.warn(err);
-    // }
-    // const readGranted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE); 
-    // const writeGranted = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE);
-
-    // if(!readGranted || !writeGranted ) {
-    //   console.log('Read and write permissions have not been granted');
-    //   return;
-    // }else
-    // {
-    //   RNFS.exists(GlowFolder).then((folderExist) => {
-    //     if (folderExist) {
-    //       //console.log('folder exist ->> ', folderExist);
-    //     } else {
-    //       RNFS.mkdir(GlowFolder)
-    //         .then((isDirectoryCreated) => {
-    //           //console.log('DirectoryCreated');
-    //         })
-    //         .catch((err) => {
-    //           console.log(err.message);
-    //         });
-    //     }
-    //   });
-    // }
 
     await PermissionFile.checkAndGrantPermission(
       err => {
@@ -144,12 +49,6 @@ class HomeScreen extends Component {
     )
   };
 
-
-  toggleSwitch = value => {
-    //onValueChange of the Admin switch this function will be called, state changes according to switch
-    console.log("Switch Value ->> ", value)
-    this.setState({ isSwitchOn: value })
-  };
 
   // Render Method
   render() {
@@ -177,38 +76,44 @@ class HomeScreen extends Component {
           <View style={styles.welcome}>
             <Text style={styles.welcomeTxt}></Text>
           </View>
-          {/* <TouchableHighlight style={styles.Create} underlayColor="#fff" onPress ={() => this.props.navigation.navigate("Session")}>
-              <Text style={[styles.ButtonTxtStyle]}>{'Create'}</Text>
-            </TouchableHighlight> */}
-
-          {/* <Text style={styles.titleCaption}>{"Select Product Type "}</Text> */}
-          <View style={{ flexDirection: 'column', alignContent: 'flex-start' , flex:0.3,marginTop:40}}>
-            {/* <Text style={styles.P5Decription}>{"Switch to P5 Product"}</Text>
-            <Switch trackColor={{ true: '#FFE800',false: Platform.OS == 'android' ? '#d3d3d3' : '#fbfbfb' }}
-              //thumbColor={'#FFD100'}
-              ios_backgroundColor="#fbfbfb"
-              onValueChange={this.toggleSwitch}
-              value={this.state.isSwitchOn}
-              style={styles.switchStyle}
-            /> */}
-              <Dropdown
-                style={styles.dropdown}
+          <View style={{ flexDirection: 'column', alignContent: 'flex-start', flex: 0.3, marginTop: 40 }}>
+            <Dropdown
                 data={this.state.arrProductType}
-                value={this.state.selectedSessionType}
-                onChangeText={(value,index) => this.setState({ selectedSessionType: value, selectedIndex: index})}
+                defaultButtonText={"CC Product"}
+                onSelect={(selectedItem, index) => {
+                  this.setState({ selectedSessionType: selectedItem.value, selectedIndex: index })
+                }}
+                disabled={this.state.arrProductType.length > 0 ? false : true}
+                buttonStyle={styles.dropdown1BtnStyle}
+                buttonTextStyle={styles.dropdown1BtnTxtStyle}
+                dropdownIconPosition={'right'}
+                dropdownStyle={styles.dropdown1DropdownStyle}
+                rowStyle={styles.dropdown1RowStyle}
+                rowTextStyle={styles.dropdown1RowTxtStyle}
+                renderDropdownIcon={isOpened => {
+                  return <FontAwesome name={isOpened ? 'caret-up' : 'caret-down'} color={'#444'} size={20} />;
+                }}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                  // text represented after item is selected
+                  // if data array is an array of objects then return selectedItem.property to render after item is selected
+                  return selectedItem.value
+                }}
+                rowTextForSelection={(item, index) => {
+                  // text represented for each item in dropdown
+                  // if data array is an array of objects then return item.property to represent item in dropdown
+                  return item.value
+                }}
               />
           </View>
-
-          {/* <TouchableHighlight style={styles.Create} underlayColor="#fff" onPress ={() => this.props.navigation.push("P5SessionScreen")}> */}
-          {/* <TouchableHighlight style={styles.Create} underlayColor="#fff" onPress={() => { this.state.isSwitchOn ? this.props.navigation.push("P5SessionScreen") : this.props.navigation.push("SessionScreen") }}> */}
-          <TouchableHighlight 
-            style={styles.Create} 
-            underlayColor="#fff" 
-            onPress={ () => { 
-              (this.state.selectedIndex  == 1)  ? this.props.navigation.push("P5SessionScreen") :
-              (this.state.selectedIndex  == 2)  ? this.props.navigation.push("SFPSessionScreen") :
-              this.props.navigation.push("SessionScreen") }
-              }>
+          <TouchableHighlight
+            style={styles.Create}
+            underlayColor="#fff"
+            onPress={() => {
+              (this.state.selectedIndex == 1) ? this.props.navigation.push("P5SessionScreen") :
+                (this.state.selectedIndex == 2) ? this.props.navigation.push("SFPSessionScreen") :
+                  this.props.navigation.push("SessionScreen")
+            }
+            }>
             <Text style={[styles.ButtonTxtStyle]}>{'Create'}</Text>
           </TouchableHighlight>
 
@@ -237,18 +142,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   Create: {
-    //marginTop: 8,
-    //marginBottom:12,
     marginLeft: 85,
     width: '50%',
     height: '8%',
     backgroundColor: Colors.cyanMain,
     borderRadius: 6,
     borderWidth: 1,
-    //borderColor: '#1bb09d',
     borderColor: Colors.lightCyan,
     justifyContent: 'center',
-    alignItems:'center'
+    alignItems: 'center'
   },
   ButtonTxtStyle: {
     //color:'#1bb09d',
@@ -301,14 +203,42 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: Colors.white,
     height: '100%',
-    textAlign:'center',
+    textAlign: 'center',
     borderRadius: 5,
     backgroundColor: "transparent",
     borderColor: '#ddd',
     borderWidth: 1,
     shadowColor: 'transparent',
-    borderBottomColor:'transparent'
+    borderBottomColor: 'transparent'
   },
+  dropdown1BtnStyle: {
+    width: 360,
+    height: 40,
+    //borderBottomWidth: 1,
+    backgroundColor: Colors.cyanMain,
+    borderColor: '#1bb09d',
+    marginRight: 0,
+    borderRadius: 6
+  },
+  dropdown1BtnTxtStyle: {
+    color: '#444',
+    textAlign: 'center',
+    fontSize: 17,
+    marginLeft: 5,
+    fontWeight:'500'
+  },
+  dropdown1DropdownStyle: {
+    backgroundColor: '#EFEFEF'
+  },
+  dropdown1RowStyle: {
+    backgroundColor: '#EFEFEF',
+    borderBottomColor: '#C5C5C5'
+  },
+  dropdown1RowTxtStyle: {
+    color: '#444',
+    textAlign: 'center',
+    fontSize: 16
+  }
 });
 
 // Make the Component available to other parts of the application
